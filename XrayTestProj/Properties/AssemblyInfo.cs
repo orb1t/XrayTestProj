9b0dc403d3daf890ab1ca37b31e4e120ff3829cf
DESKTOP-IRBKFCU\user <denisg@123software.ru>
2018-11-28 14:54:12 +0200

    test

---
+++ XrayTestProj/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle( "XrayTestProj" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "XrayTestProj" )]
+[assembly: AssemblyCopyright( "Copyright ©  2018" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+[assembly: ComVisible( false )]
+
+[assembly: Guid( "9d757963-8755-4f3c-8360-149b2104a551" )]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
