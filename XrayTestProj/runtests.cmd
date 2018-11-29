df1f5a6429fdc56b78cc36a8674b76fef8700e88
DESKTOP-IRBKFCU\user <denisg@123software.ru>
2018-11-29 13:40:00 +0200

    test

--- XrayTestProj/runtests.cmd
+++ XrayTestProj/runtests.cmd
@@ -6,7 +6,7 @@ rem @IF ERRORLEVEL 1 (
 rem 	echo "MSBuild is not in your PATH. Please use a developer command prompt!"
 rem 	goto :end
 rem ) ELSE (
-	MSBuild.exe "XrayTestProj.csproj"
+	C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe "XrayTestProj.csproj"
 rem )
 
 @if ERRORLEVEL 1 goto end
