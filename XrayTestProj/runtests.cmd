f56787c599a4cd8d782c0a209e8d7b0ddc99c89f
DESKTOP-IRBKFCU\user <denisg@123software.ru>
2018-11-29 14:17:08 +0200

    test

--- XrayTestProj/runtests.cmd
+++ XrayTestProj/runtests.cmd
@@ -1,5 +1,13 @@
 @pushd %~dp0
 
+del ..\TestResults\*.*
+del *.feature.cs
+del *.feature
+del .\bin\*.*
+
+..\packages\Acheve.Jira.XRay.0.0.3\tools\Acheve.Jira.XRay.exe get-features -h http://10.1.133.215:8181 -u denisg -p 123qwe -t XRAYT0-1829 -d ./
+rename 1.feature 16.feature
+
 rem @where /q msbuild
 
 rem @IF ERRORLEVEL 1 (
@@ -17,6 +25,8 @@ rem )
 @if "%profile%" == "" set profile=Default
 
 SpecRun.exe run %profile%.srprofile "/baseFolder:%~dp0\bin\Debug" /log:specrun.log %2 %3 %4 %5
+
+..\..\Acheve.Jira.XRay.0.0.3\tools\Acheve.Jira.XRay.exe upload-results -h http://10.1.133.215:8181 -u denisg -p 123qwe -t XRAYT0-1829 -f ..\..\..\XrayTestProj\bin\Debug\data.json
 
 :end
 
